493b1b27d8c40111e40b17c196769d414d78d491
behzad <behsoft@outlook.com>
2025-06-03 19:34:16 +0200

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    safelist: [
+      'border-blue-500',
+      'border-red-500',
+      'border-green-500',
+      'border-yellow-500',
+      'border-purple-500',
+      'border-pink-500',
+      'border-orange-500',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
