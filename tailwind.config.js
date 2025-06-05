5a6bb204939551038771f5bcd167595c025600af
behzad <behsoft@outlook.com>
2025-06-05 18:25:33 +0200

    tailwind and uuid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   safelist: [
-    'border-blue-500',
-    'border-red-500',
-    'border-green-500',
-    'border-yellow-500',
-    'border-purple-500',
-    'border-pink-500',
-    'border-orange-500',
+    'bg-blue-500',
+    'bg-green-500',
+    'bg-red-500',
+    'bg-yellow-500',
+    'bg-purple-500',
+    'bg-pink-500',
+    'bg-orange-500',
   ],
   theme: {
     extend: {},
